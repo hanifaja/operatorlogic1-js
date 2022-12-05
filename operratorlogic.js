ee8111693596b04647998100978baf15d5abfc98
hanifaja <nurdiyantor21064@gmail.com>
2022-12-05 20:30:19 +0700

    initial commit

---
+++ operratorlogic.js
@@ -0,0 +1,69 @@
+//true dan false
+let number = 1;
+while(number <=10){
+    console.log(number);
+    if(number < 5){
+      console.log(true);
+    }
+    else{
+      console.log(false);
+    }
+    number ++;
+  }
+
+/*
+let number = angka dimulai dari urutan 1;
+while(number <=10) = while= mengulang, number <=10= kurang dari sama dengan sepuluh,jadi mengulang kurang dari sama dengan sepuluh
+console.log = untuk menapilkan while(number <=10)
+number ++= nilai awal index ditambah dengan 1.
+if= jika number kurang dari sama dengan akan mmengeluarkan tulisan true
+else= menghasilkan tulisan false
+*/
+
+   //ganjil dan genap ada angka nya pakai while
+//  let number = 1;
+// while(number <=20){
+//     console.log(number);
+  
+//   if (number % 2 === 0) {
+//     console.log("genap");
+//   } 
+//   else {
+//     console.log("ganjil");
+//   }
+//   number ++;
+// }
+
+/*
+let number = angka dimulai dari urutan 1;
+while(number <=20) = while= mengulang, number <=20= kurang dari sama dengan dua puluh,jadi mengulang kurang dari sama dengan dua puluh
+console.log = untuk menapilkan while(number <=10)
+number ++= nilai awal index ditambah dengan 1.
+if= (number %2 ===0 arti mya %= sisa bagi,===  sama dengan) jika number kurang dibagi 2 sama dengan 0 menghasilkan tulisan ganjil
+else= menghasilkan tulisan genap
+*/
+
+//angka random stop dan lanjut
+
+ /*let a = "lanjut";
+for( let b = 0; b < 10; b++){
+  let number = Math.floor(Math.random() * 10);
+  number ++
+  if(number === 1){
+    a = "stop"
+   }
+   console.log(number)
+   console.log(a)
+  }
+
+  /* 
+  let a = "lanjut", karena angka pertama selain angka 1 akan menghasilkan tulisan lanjut.
+  ( let b = 0; b < 10; b++) = untuk variabel b adalah 0, dan b kurang dari 10, tambah dan kembalikan variable a.
+  let number = math.floor(math.random() * 10); = adalah menghasilkan angka acak dan random.
+  number++ = nilai awal index ditambah dengan 1.
+  if (number === 1){
+    a = "stop"
+  } adalah jika menghasikkan angka satu maka akan muncul tulisan stop dan seterusnya dibawah angka satu.
+console.log(number) = memunculkan number.
+console.log(a) = memunculakan antara "lanjut" dan "stop".
+  */
